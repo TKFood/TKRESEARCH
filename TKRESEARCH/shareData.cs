8be50a347a92b106b8a58db0723b21c406753e0b
at3158 <tk290@tkfood.com.tw>
2022-06-13 14:23:39 +0800

    FrmParent_Load

---
+++ TKRESEARCH/shareData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace TKRESEARCH
+{    public class shareArea
+    {
+        //--------------------------------------------
+        // 以下之shareData宣告為靜態變數，隸屬類別層級，
+        // 調用時可直接透過[類別.變數名稱]即可
+        //--------------------------------------------
+        public static string shareData;
+        public static string UserName;
+    }
+}
