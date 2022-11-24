2e529240a07b042bac08a7944840e5be2929174e
at3158 <tk290@tkfood.com.tw>
2022-11-24 10:04:39 +0800

    add FrmINVMBCALCOST

---
+++ TKRESEARCH/FrmINVMBCALCOST.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using FastReport;
+using FastReport.Data;
+using System.Collections;
+using TKITDLL;
+
+namespace TKRESEARCH
+{
+    public partial class FrmINVMBCALCOST : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter1 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+
+ 
+
+
+        DataSet ds = new DataSet();
+      
+        int rownum = 0;
+        int result;
+    
+
+        string ID;
+
+
+        public FrmINVMBCALCOST()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+
+        #endregion
+
+        #region BUTTON
+
+        #endregion
+    }
+}
