cb810c2fb6b58d53ffd3d46fb66d0f36ec8366bb
at3158 <tk290@tkfood.com.tw>
2022-12-12 16:22:04 +0800

    public void SETdataGridView2()

--- TKRESEARCH/FrmRESEARCHDB.cs
+++ TKRESEARCH/FrmRESEARCHDB.cs
@@ -61,6 +61,8 @@ namespace TKRESEARCH
 
             SEARCH(textBox1A.Text.Trim());
             SETdataGridView1();
+            SEARCH2(textBox2A.Text.Trim());
+            SETdataGridView2();
         }
 
         #region FUNCTION
@@ -156,6 +158,126 @@ namespace TKRESEARCH
 
         }
 
+        public void SEARCH2(string KEYS)
+        {
+            SqlDataAdapter adapter1 = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+
+            DataSet ds1 = new DataSet();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sbSql.Clear();
+
+                if (!string.IsNullOrEmpty(KEYS))
+                {
+                    sbSql.AppendFormat(@"  
+                                        SELECT 
+                                        [ID]
+                                        ,[NAMES] AS '產品品名'
+                                        ,[CHARS] AS '產品特色'
+                                        ,[ORIS] AS '產品成分'
+                                        ,[SPECS] AS '產品規格'
+                                        ,[PRICES] AS '售價'
+                                        ,[SAVEDAYS] AS '保存期限'
+                                        ,[SAVEMETHODS] AS '保存方式'
+                                        ,[PRIMES] AS '素別'
+                                        ,[ALLERGENS] AS '過敏原'
+                                        ,[OWNERS] AS '負責廠商'
+                                        ,[EATES] AS '建議食用方式'
+                                        ,[CHECKSUNITS] AS '驗證單位'
+                                        ,[CHECKS] AS '驗證證書字號'
+                                        ,[OTHERS] AS '其他口味'
+                                        ,[COMMENTS] AS '備註'
+                                        ,CONVERT(NVARCHAR,[CREATEDATES],112) AS '填表日期'
+                                        ,[DOCNAMES] AS '產品圖片'
+                                        ,[CONTENTTYPES] 
+                                        ,[DATAS] 
+                                        FROM [TKRESEARCH].[dbo].[TBDB2]
+                                        WHERE [NAMES] LIKE '%{0}%'
+                                        ORDER BY [ID] 
+
+                                    ", KEYS);
+                }
+                else
+                {
+                    sbSql.AppendFormat(@"                                          
+                                        SELECT 
+                                        [ID]
+                                        ,[NAMES] AS '產品品名'
+                                        ,[CHARS] AS '產品特色'
+                                        ,[ORIS] AS '產品成分'
+                                        ,[SPECS] AS '產品規格'
+                                        ,[PRICES] AS '售價'
+                                        ,[SAVEDAYS] AS '保存期限'
+                                        ,[SAVEMETHODS] AS '保存方式'
+                                        ,[PRIMES] AS '素別'
+                                        ,[ALLERGENS] AS '過敏原'
+                                        ,[OWNERS] AS '負責廠商'
+                                        ,[EATES] AS '建議食用方式'
+                                        ,[CHECKSUNITS] AS '驗證單位'
+                                        ,[CHECKS] AS '驗證證書字號'
+                                        ,[OTHERS] AS '其他口味'
+                                        ,[COMMENTS] AS '備註'
+                                        ,CONVERT(NVARCHAR,[CREATEDATES],112) AS '填表日期'
+                                        ,[DOCNAMES] AS '產品圖片'
+                                        ,[CONTENTTYPES] 
+                                        ,[DATAS] 
+                                        FROM [TKRESEARCH].[dbo].[TBDB2]
+                                        FROM [TKRESEARCH].[dbo].[TBDB1]
+                                        ORDER BY [ID] 
+                                    ");
+                }
+
+
+                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
+                sqlConn.Open();
+                ds1.Clear();
+                adapter1.Fill(ds1, "ds1");
+                sqlConn.Close();
+
+
+                if (ds1.Tables["ds1"].Rows.Count >= 1)
+                {
+                    dataGridView2.DataSource = ds1.Tables["ds1"];
+
+                    dataGridView2.AutoResizeColumns();
+
+
+
+                }
+                else
+                {
+                    dataGridView2.DataSource = null;
+
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
         //設定下載欄
         public void SETdataGridView1()
         {
@@ -168,46 +290,6 @@ namespace TKRESEARCH
 
             dataGridView1.Columns.Insert(dataGridView1.ColumnCount, lnkDownload);
             dataGridView1.CellContentClick += new DataGridViewCellEventHandler(DataGridView1_CellClick);
-        }
-        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
-        {
-            textBox1B.Text = null;
-            textBox1C.Text = null;
-            textBox14.Text = null;
-            textBox15.Text = null;
-
-            if (dataGridView1.CurrentRow != null)
-            {
-                int rowindex = dataGridView1.CurrentRow.Index;
-
-                if (dataGridView1.CurrentCell.RowIndex > 0 || dataGridView1.CurrentCell.ColumnIndex > 0)
-                {
-                    ROWSINDEX = dataGridView1.CurrentCell.RowIndex;
-                    COLUMNSINDEX = dataGridView1.CurrentCell.ColumnIndex;
-
-                    rowindex = ROWSINDEX;                }
-
-
-                if (rowindex >= 0)
-                {
-                    DataGridViewRow row = dataGridView1.Rows[rowindex];
-                    textBox1B.Text = row.Cells["ID"].Value.ToString();
-                    textBox1C.Text = row.Cells["ID"].Value.ToString();
-                    textBox14.Text = row.Cells["文件編號"].Value.ToString();
-                    textBox15.Text = row.Cells["備註"].Value.ToString();
-
-
-                }
-                else
-                {
-                    textBox1B.Text = null;
-                    textBox1C.Text = null;
-                    textBox14.Text = null;
-                    textBox15.Text = null;
-
-                }
-            }
-
         }
         private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -282,6 +364,147 @@ namespace TKRESEARCH
                 }
             }
         }
+
+        public void SETdataGridView2()
+        {
+            DataGridViewLinkColumn lnkDownload = new DataGridViewLinkColumn();
+            lnkDownload.UseColumnTextForLinkValue = true;
+            lnkDownload.LinkBehavior = LinkBehavior.SystemDefault;
+            lnkDownload.Name = "lnkDownload";
+            lnkDownload.HeaderText = "Download";
+            lnkDownload.Text = "Download";
+
+            dataGridView2.Columns.Insert(dataGridView2.ColumnCount, lnkDownload);
+            dataGridView2.CellContentClick += new DataGridViewCellEventHandler(DataGridView2_CellClick);
+        }
+        private void DataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            StringBuilder SQL = new StringBuilder();
+
+            DataGridView dgv = (DataGridView)sender;
+            string columnName = dgv.Columns[e.ColumnIndex].Name;
+
+            if (e.RowIndex >= 0 && columnName.Equals("lnkDownload"))
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                int ID = Convert.ToInt16((row.Cells["ID"].Value));
+                byte[] bytes;
+                string fileName, contentType;
+
+                // 20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+                using (SqlConnection con = sqlConn)
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        SQL.AppendFormat(@"
+                                            SELECT 
+                                            [ID]
+                                            ,[NAMES]
+                                            ,[CHARS]
+                                            ,[ORIS]
+                                            ,[SPECS]
+                                            ,[PRICES]
+                                            ,[SAVEDAYS]
+                                            ,[SAVEMETHODS]
+                                            ,[PRIMES]
+                                            ,[ALLERGENS]
+                                            ,[OWNERS]
+                                            ,[EATES]
+                                            ,[CHECKSUNITS]
+                                            ,[CHECKS]
+                                            ,[OTHERS]
+                                            ,[COMMENTS]
+                                            ,[CREATEDATES]
+                                            ,[DOCNAMES]
+                                            ,[CONTENTTYPES]
+                                            ,[DATAS]
+                                            FROM [TKRESEARCH].[dbo].[TBDB2]
+                                            WHERE ID=@ID
+                                            ORDER BY [ID] 
+                                       
+                                            ");
+                        cmd.CommandText = SQL.ToString();
+                        cmd.Parameters.AddWithValue("@ID", ID);
+                        cmd.Connection = con;
+                        con.Open();
+
+                        using (SqlDataReader sdr = cmd.ExecuteReader())
+                        {
+                            sdr.Read();
+                            bytes = (byte[])sdr["DATAS"];
+                            contentType = sdr["CONTENTTYPES"].ToString();
+                            fileName = sdr["DOCNAMES"].ToString();
+
+                            Stream stream;
+                            SaveFileDialog saveFileDialog = new SaveFileDialog();
+                            saveFileDialog.Filter = "All files (*.*)|*.*";
+                            saveFileDialog.FilterIndex = 1;
+                            saveFileDialog.RestoreDirectory = true;
+                            saveFileDialog.FileName = fileName;
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                stream = saveFileDialog.OpenFile();
+                                stream.Write(bytes, 0, bytes.Length);
+                                stream.Close();
+                            }
+                        }
+                    }
+                    con.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            textBox1B.Text = null;
+            textBox1C.Text = null;
+            textBox14.Text = null;
+            textBox15.Text = null;
+
+            if (dataGridView1.CurrentRow != null)
+            {
+                int rowindex = dataGridView1.CurrentRow.Index;
+
+                if (dataGridView1.CurrentCell.RowIndex > 0 || dataGridView1.CurrentCell.ColumnIndex > 0)
+                {
+                    ROWSINDEX = dataGridView1.CurrentCell.RowIndex;
+                    COLUMNSINDEX = dataGridView1.CurrentCell.ColumnIndex;
+
+                    rowindex = ROWSINDEX;                }
+
+
+                if (rowindex >= 0)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[rowindex];
+                    textBox1B.Text = row.Cells["ID"].Value.ToString();
+                    textBox1C.Text = row.Cells["ID"].Value.ToString();
+                    textBox14.Text = row.Cells["文件編號"].Value.ToString();
+                    textBox15.Text = row.Cells["備註"].Value.ToString();
+
+
+                }
+                else
+                {
+                    textBox1B.Text = null;
+                    textBox1C.Text = null;
+                    textBox14.Text = null;
+                    textBox15.Text = null;
+
+                }
+            }
+
+        }
+       
 
     
 
@@ -509,9 +732,13 @@ namespace TKRESEARCH
                 //do something else
             }
         }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SEARCH2(textBox2A.Text.Trim());
+        }
 
         #endregion
 
-      
+
     }
 }
