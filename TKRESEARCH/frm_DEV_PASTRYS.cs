67c6e244cc494c3ff8f6a4af8d80b9b8624faa03
at3158 <tk290@tkfood.com.tw>
2024-02-20 09:55:17 +0800

    button2_Click

---
+++ TKRESEARCH/frm_DEV_PASTRYS.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using FastReport;
+using FastReport.Data;
+using System.Net.Mail;
+using TKITDLL;
+
+namespace TKRESEARCH
+{
+    public partial class Frm_DEV_PASTRYS : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+        SqlTransaction tran;
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string talbename = null;
+        int rownum = 0;
+        int result;
+        string DATES = null;
+        string DirectoryNAME = null;
+        string pathFileSALESMONEYS = null;
+        string NO = "";
+
+        public Frm_DEV_PASTRYS()
+        {
+            InitializeComponent();
+        }
+
+
+        #region FUNCTION
+        public void SEARCH_TB_DEV_PASTRYS(string NO,string NAMES)
+        {
+            StringBuilder sbSql = new StringBuilder();
+            StringBuilder sbSqlQuery = new StringBuilder();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+            DataSet ds = new DataSet();
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+                StringBuilder SQLquery1 = new StringBuilder();
+                StringBuilder SQLquery2 = new StringBuilder();
+
+                if(!string.IsNullOrEmpty(NO))
+                {
+                    SQLquery1.AppendFormat(@" AND CONVERT(NVARCHAR,[DEVCARESTEDATES],112) LIKE '%{0}%'", NO);
+                }
+                else
+                {
+                    SQLquery1.AppendFormat(@" ");
+                }
+                if (!string.IsNullOrEmpty(NAMES))
+                {
+                    SQLquery2.AppendFormat(@" AND [NAMES] LIKE '%{0}%'", NAMES);
+                }
+                else
+                {
+                    SQLquery2.AppendFormat(@" ");
+                }
+
+                sbSql.Clear();
+
+                sbSql.AppendFormat(@"  
+                                   SELECT 
+                                    [NO] AS '編號'
+                                    ,[NAMES] AS '產品名稱 '
+                                    ,[SPECS] AS '規格(g) '
+                                    ,CONVERT(NVARCHAR,[DEVCARESTEDATES],112) AS '開發日期 '
+                                    ,[BEFROECOOKEDSPCS] AS '烤前長*寬*厚(cm) '
+                                    ,[AFERCOOKEDSPCS] AS '烤後長*寬*厚(cm) '
+                                    ,[BEFORECOOKEDWEIGHTS] AS '烤前重量(g) '
+                                    ,[AFTERCOOKEDWEIGHTS] AS '烤後重量(g) '
+                                    ,[COOKEDTEMP] AS '烘焙溫度(℃) '
+                                    ,[COOKEDTIMES] AS '烘焙時間(m) '
+                                    ,[TOTALS] AS '總產量(片or公斤)'
+                                    ,[THICKNESS] AS '延壓厚度(cm)'
+                                    ,[PCTS] AS '配比(水麵:油酥)'
+                                    ,[COMMETNS] AS '工作流程'
+                                    ,[ID] 
+                                    FROM [TKRESEARCH].[dbo].[TB_DEV_PASTRYS]
+                                    WHERE 1=1
+                                    {0}
+                                    {1}
+                                    ORDER BY [NO]
+                                    ", SQLquery1.ToString(), SQLquery2.ToString());
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "ds");
+                sqlConn.Close();
+
+
+                if (ds.Tables["ds"].Rows.Count == 0)
+                {
+                    dataGridView1.DataSource = null;
+
+                }
+                else
+                {
+                    if (ds.Tables["ds"].Rows.Count >= 1)
+                    {
+                        dataGridView1.DataSource = ds.Tables["ds"];
+                        dataGridView1.AutoResizeColumns();
+                    }
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            NO = "";
+
+            if (dataGridView1.CurrentRow != null)
+            {
+                int rowindex = dataGridView1.CurrentRow.Index;
+                DataGridViewRow row = dataGridView1.Rows[rowindex];
+
+                NO = row.Cells["編號"].Value.ToString();
+                SEARCH_TB_DEV_PASTRYS_DETAILS(NO);
+
+            }
+            
+        }
+
+        public void SEARCH_TB_DEV_PASTRYS_DETAILS(string NO)
+        {
+            StringBuilder sbSql = new StringBuilder();
+            StringBuilder sbSqlQuery = new StringBuilder();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();   
+            DataSet ds = new DataSet();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+                sbSql.Clear();
+
+                sbSql.AppendFormat(@"  
+                                  SELECT
+                                    [NO] AS '編號'
+                                    ,[KINDS] AS '品項'
+                                    ,[SEQ] AS '投料順序'
+                                    ,[CODE] AS '代號'
+                                    ,[SUPPLIERS] AS '供應商'
+                                    ,[NAMES] AS '原料品項'
+                                    ,[PCTS] AS '各自百分比(%)'
+                                    ,[WEIGHTS] AS '各自重量(g)'
+                                    ,[TPCTS] AS '加總後百分比(%)'
+                                    ,[TWEIGHTS] AS '加總後重量(g)'
+                                    , [ID]
+                                    FROM [TKRESEARCH].[dbo].[TB_DEV_PASTRYS_DETAILS]
+                                    WHERE [NO]='{0}'
+                                    ORDER BY [KINDS],[CODE]
+                                  
+                                    ",NO );
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "ds");
+                sqlConn.Close();
+
+
+                if (ds.Tables["ds"].Rows.Count == 0)
+                {
+                    dataGridView2.DataSource = null;
+                }
+                else
+                {
+                    if (ds.Tables["ds"].Rows.Count >= 1)
+                    {
+                        dataGridView2.DataSource = ds.Tables["ds"];
+                        dataGridView2.AutoResizeColumns();
+                    }
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SEARCH_TB_DEV_PASTRYS(dateTimePicker1.Value.ToString("yyyy"),textBox1.Text);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 或者使用 SelectedTab 屬性直接指定 Tab 頁面物件
+            tabControl1.SelectedTab = tabPage2;
+
+            //MessageBox.Show(NO);
+        }
+
+        #endregion
+
+       
+    }
+}
