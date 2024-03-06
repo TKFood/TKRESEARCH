6e26276abeca7a820128aa69aa8fba8ef0bd6aeb
at3158 <tk290@tkfood.com.tw>
2024-03-06 15:44:21 +0800

    comboBox1load

--- TKRESEARCH/Frm_TB_DEV_BREADS.cs
+++ TKRESEARCH/Frm_TB_DEV_BREADS.cs
@@ -52,10 +52,48 @@ namespace TKRESEARCH
         public Frm_TB_DEV_BREADS()
         {
             InitializeComponent();
+
+            comboBox1load();
         }
 
         #region FUNCTION
+        public void comboBox1load()
+        {
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
 
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            StringBuilder Sequel = new StringBuilder();
+            Sequel.AppendFormat(@"
+                                SELECT [ID]
+                                ,[KIND]
+                                ,[PARAID]
+                                ,[PARANAME]
+                                FROM [TKRESEARCH].[dbo].[TBPARA]
+                                WHERE [KIND]='TB_DEV_BREADS'
+                                ORDER BY [PARAID]
+                                ");
+            SqlDataAdapter da = new SqlDataAdapter(Sequel.ToString(), sqlConn);
+            DataTable dt = new DataTable();
+            sqlConn.Open();
+
+            dt.Columns.Add("PARANAME", typeof(string));
+
+            da.Fill(dt);
+            comboBox1.DataSource = dt.DefaultView;
+            comboBox1.ValueMember = "PARANAME";
+            comboBox1.DisplayMember = "PARANAME";
+            sqlConn.Close();
+
+
+        }
 
         #endregion
 
