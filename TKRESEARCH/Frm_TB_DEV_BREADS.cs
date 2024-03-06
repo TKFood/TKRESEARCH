17273d360ad51acf0473d699baf2df6b9899ff99
at3158 <tk290@tkfood.com.tw>
2024-03-06 14:43:13 +0800

    Frm_TB_DEV_BREADS

---
+++ TKRESEARCH/Frm_TB_DEV_BREADS.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using FastReport;
+using FastReport.Data;
+using System.Net.Mail;
+using TKITDLL;
+using System.Globalization;
+using System.Collections;
+
+namespace TKRESEARCH
+{
+    public partial class Frm_TB_DEV_BREADS : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+        SqlTransaction tran;
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string talbename = null;
+        int rownum = 0;
+        int result;
+        string DATES = null;
+        string DirectoryNAME = null;
+        string pathFileSALESMONEYS = null;
+        string NO = "";
+
+        public Frm_TB_DEV_BREADS()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
