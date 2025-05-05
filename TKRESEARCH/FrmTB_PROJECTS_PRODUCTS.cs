ea6a891f0a22d4fc1d31769bb23ab8ffc90f5489
at3158 <tk290@tkfood.com.tw>
2025-05-05 16:51:35 +0800

    FrmTB_PROJECTS_PRODUCTS

---
+++ TKRESEARCH/FrmTB_PROJECTS_PRODUCTS.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using FastReport;
+using FastReport.Data;
+using System.Net.Mail;
+using TKITDLL;
+using System.Globalization;
+using System.Collections;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TKRESEARCH
+{
+    public partial class FrmTB_PROJECTS_PRODUCTS : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+        SqlTransaction tran;
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string talbename = null;
+        int rownum = 0;
+        int result;
+
+        public FrmTB_PROJECTS_PRODUCTS()
+        {
+            InitializeComponent();
+        }
+
+
+        #region FUNCTION
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
